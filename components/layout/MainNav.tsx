1c1cd6bfffbbbd8f494ad97db8222b8365b4c1b6
박지수 <jisupark2000@gmail.com>
2022-06-16 00:53:46 +0900

    메인 네비게이션 기본 레이아웃 잡기

--- components/layout/MainNav.tsx
+++ components/layout/MainNav.tsx
@@ -1,5 +1,23 @@
+import Link from 'next/link';
+import styles from './MainNav.module.scss';
+import ModeChangeBtn from './ModeChageBtn';
+
 const MainNav = () => {
-  return <div></div>;
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.innerNav}>
+        <div className={styles.logo}>
+          <Link href='/'>
+            <a>큰거온다</a>
+          </Link>
+        </div>
+        <div className={styles.btns}>
+          <ModeChangeBtn />
+          <button className={styles['new-post-btn']}>New</button>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default MainNav;
---
+++ components/layout/ModeChageBtn.tsx
@@ -0,0 +1,7 @@
+import styles from './ModeChageBtn.module.scss';
+
+const ModeChangeBtn = () => {
+  return <button className={styles.btn}>😆</button>;
+};
+
+export default ModeChangeBtn;
