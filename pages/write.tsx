461755faa57c127729eaaf63b958562be9631938
박지수 <jisupark2000@gmail.com>
2022-06-16 02:12:10 +0900

    새 게시글 작성 파일 기본 설정

---
+++ pages/write.tsx
@@ -0,0 +1,11 @@
+import styles from './Write.module.scss';
+
+const Write = () => {
+  return (
+    <div className={styles.wrapper}>
+      <textarea className={styles.title}></textarea>
+    </div>
+  );
+};
+
+export default Write;
