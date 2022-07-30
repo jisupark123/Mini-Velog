cd146947468a9ae52fe60b1116814ddf98a09ebc
박지수 <jisupark2000@gmail.com>
2022-07-30 10:45:28 +0900

    create session function

---
+++ lib/secret/createSession.ts
@@ -0,0 +1,9 @@
+export function createSession(userId: number) {
+  const randomString = Math.random().toString(36).slice(2);
+  const time = new Date().getTime();
+  return `userId=${userId}&rs=${randomString + time}`;
+}
+
+export function getUserIdFromSession(sessionId: number) {
+  return +String(sessionId).split('userId=')[1].split('&')[0];
+}
